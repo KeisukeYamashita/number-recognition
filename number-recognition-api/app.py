7fae86a48c65db83d86aa45e18f0c0a94b063592
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-11-26 22:59:47 +0900

    Train neural network when app starts

--- number-recognition-api/app.py
+++ number-recognition-api/app.py
@@ -1,21 +1,24 @@
 # coding: utf-8
-from flask import Flask
-from two_layer_net import TwoLayerNet
-from train_neural_network import train_neural_network
+from flask import Flask, jsonify
+from trainer import Trainer
 
 
 app = Flask(__name__)
+trainer = Trainer()
 
 
 @app.route('/train', methods=['GET'])
 def train():
-    return 'train'
+    trainer.train()
+    return jsonify({ 'train_status': trainer.train_status })
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # trainer.network.predict()
     return 'predict'
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=3000)
+    trainer.train()
