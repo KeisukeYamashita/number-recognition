d4f7279b33e6159566df151e854c5e37cde8c68b
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-11-26 19:26:49 +0900

    Add module to load mnist dataset

--- number-recognition-api/main.py
+++ number-recognition-api/main.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/train', methods=['GET'])
 def train():
     return 'train'
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     return 'predict'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=3000)
