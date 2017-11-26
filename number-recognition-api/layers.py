34ab460f55dab50836c313ab6ac0a589cb4b3e94
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-11-26 20:25:56 +0900

    Add layers

---
+++ number-recognition-api/layers.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+import numpy as np
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        return dout
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+
+        self.original_x_shape = None
+
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+        out = np.dot(self.x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = dx.reshape(*self.original_x_shape)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = self._softmax(x)
+        self.loss = self._cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    @staticmethod
+    def _softmax(x):
+        if x.ndim == 2:
+            x = x.T
+            x = x - np.max(x, axis=0)
+            y = np.exp(x) / np.sum(np.exp(x), axis=0)
+            return y.T
+
+        x = x - np.max(x)
+        return np.exp(x) / np.sum(np.exp(x))
+
+    @staticmethod
+    def _cross_entropy_error(y, t):
+        if y.ndim == 1:
+            t = t.reshape(1, t.size)
+            y = y.reshape(1, y.size)
+
+        if t.size == y.size:
+            t = t.argmax(axis=1)
+
+        batch_size = y.shape[0]
+        return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
