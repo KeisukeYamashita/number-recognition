fb975021d477f8338dbe907d02348ba1e5fd9123
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-11-28 03:32:35 +0900

    Create ios project

---
+++ number-recognition-ios/number-recognition-ios/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  number-recognition-ios
+//
+//  Created by Masaya Hayashi on 2017/11/28.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
